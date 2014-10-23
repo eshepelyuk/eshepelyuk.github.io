f15e2f43b6ab2b1a222e807ab6e58bce4a38f11a
Ievgenii.Shepeliuk <I.Shepeliuk@external.oberthur.com>
2014-10-23 14:45:41 +0300

    jekyll

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #/bin/bash
-set -e
-echo "-------------"
-ls -la .
-jekyll build
-git checkout -B master
-git reset --hard
-git status
-echo "-------------"
-ls -la _site
-echo "-------------"
-ls -la .
+cd ..
+pwd
+# set -e
+# echo "-------------"
+# ls -la .
+# jekyll build
+# git checkout -B master
+# git reset --hard
+# git status
+# echo "-------------"
+# ls -la _site
+# echo "-------------"
+# ls -la .
 #cp -R _site/* .
 #git push origin master
  
