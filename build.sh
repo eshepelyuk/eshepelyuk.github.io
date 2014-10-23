4b2100a26333df975e0e0a36b5c401d55d25e8e9
Ievgenii.Shepeliuk <I.Shepeliuk@external.oberthur.com>
2014-10-23 14:49:48 +0300

    jekyll

--- build.sh
+++ build.sh
@@ -1,17 +1,12 @@
 #/bin/bash
-cd ..
-pwd
-# set -e
-# echo "-------------"
-# ls -la .
-# jekyll build
-# git checkout -B master
-# git reset --hard
-# git status
-# echo "-------------"
-# ls -la _site
-# echo "-------------"
-# ls -la .
-#cp -R _site/* .
+set -e
+jekyll build
+rm -rf ../eshepelyuk.github.io.master
+git clone --depth=50 --branch=master git://github.com/eshepelyuk/eshepelyuk.github.io.git eshepelyuk/eshepelyuk.github.io ../eshepelyuk.github.io.master
+cp -R _site/* ../eshepelyuk.github.io.master
+
+cd ../eshepelyuk.github.io.master
+ls -la .
+git status
 #git push origin master
  
