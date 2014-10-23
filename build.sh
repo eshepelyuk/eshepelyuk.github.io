7a0f083e972c0fedf6838052cef355668dd0780a
Ievgenii.Shepeliuk <I.Shepeliuk@external.oberthur.com>
2014-10-23 15:18:38 +0300

    jekyll

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 jekyll build
 rm -rf ../eshepelyuk.github.io.master
-git clone git://github.com/eshepelyuk/eshepelyuk.github.io.git ../eshepelyuk.github.io.master
+git clone https://github.com/eshepelyuk/eshepelyuk.github.io.git ../eshepelyuk.github.io.master
 cp -R _site/* ../eshepelyuk.github.io.master
 cd ../eshepelyuk.github.io.master
 ls -la .
