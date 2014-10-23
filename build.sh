fc345ab657adb8ed1ad3a968ac43f285faf23f65
Ievgenii.Shepeliuk <I.Shepeliuk@external.oberthur.com>
2014-10-23 15:45:05 +0300

    jekyll

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 jekyll build
 rm -rf ../eshepelyuk.github.io.master
-git clone https://github.com/eshepelyuk/eshepelyuk.github.io.git ../eshepelyuk.github.io.master
+git clone https://${GH_TOKEN}@github.com/eshepelyuk/eshepelyuk.github.io.git ../eshepelyuk.github.io.master
 cp -R _site/* ../eshepelyuk.github.io.master
 cd ../eshepelyuk.github.io.master
 ls -la .
@@ -15,7 +15,5 @@ git config user.name "Evgeny Shepelyuk"
 
 git add .
 git status
-git commit -a -m 'jekyll'
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push origin master
-
- 
