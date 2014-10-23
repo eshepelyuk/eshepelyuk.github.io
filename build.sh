1bddf508b60304d4f2b5ca62a00b8400a81d3508
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2014-10-23 22:03:26 +0300

    jekyll

--- build.sh
+++ build.sh
@@ -1,19 +1,18 @@
 #/bin/bash
 set -e
+
+# build site
 jekyll build
 
-# create folders and clone `master' branch
+# clone `master' branch to different folder
 rm -rf ../eshepelyuk.github.io.master
 git clone https://${GH_TOKEN}@github.com/eshepelyuk/eshepelyuk.github.io.git ../eshepelyuk.github.io.master
+
 # copy generated HTML site
 cp -R _site/* ../eshepelyuk.github.io.master
 
-cd ../eshepelyuk.github.io.master
-ls -la .
-
-#rm -rf Gemfile Gemfile.lock build.sh
-
 # commit and push
+cd ../eshepelyuk.github.io.master
 git config user.email "eshepelyuk@gmail.com"
 git config user.name "Evgeny Shepelyuk"
 git add -A .
