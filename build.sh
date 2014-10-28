e8d989b5a10436e1e8a8f6b1bece6e8f27954c3a
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2014-10-28 21:23:08 +0200

    jekyll

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
 #/bin/bash
-set -e
+# enable error reporting to the console
+set -e 
 
-# build site
+# build site with `jekyll', by default to `_site' folder
 jekyll build
 
-# clone `master' branch to different folder
+# cleanup
 rm -rf ../eshepelyuk.github.io.master
+
+#clone `master' branch of the repository using encrypted GH_TOKEN for authentification
 git clone https://${GH_TOKEN}@github.com/eshepelyuk/eshepelyuk.github.io.git ../eshepelyuk.github.io.master
 
-# copy generated HTML site
+# copy generated HTML site to `master' branch
 cp -R _site/* ../eshepelyuk.github.io.master
 
-# commit and push
+# commit and push generated content to `master' branch
+# since repository was cloned in write mode with token auth - we can push there
 cd ../eshepelyuk.github.io.master
 git config user.email "eshepelyuk@gmail.com"
 git config user.name "Evgeny Shepelyuk"
