eee989f49f0e43017250a1cc1e5c49620751084d
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2021-03-09 19:07:36 +0200

    fix

--- push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-git add -A . && git commit -a -m jekyll
-git push origin jekyll && git status
--- serve.sh
+++ serve.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 rm -rf _site
-# bundle exec jekyll serve --config _config.yml,_config_dev.yml -D -I
 
 bundle exec jekyll build --config _config.yml,_config_dev.yml
-asciidoctor-pdf EvgenyShepelyukCV.adoc -D _site/
+asciidoctor-pdf *CV.adoc -D _site/
 
 start _site/index.html
