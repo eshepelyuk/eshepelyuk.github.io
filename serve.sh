45ff858350fe8a45587fdb3b0e372ac1d26527c1
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2021-03-09 18:03:51 +0200

    fix

--- serve.sh
+++ serve.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-bundle exec jekyll serve --config _config.yml,_config_dev.yml -D -I
+rm -rf _site
+# bundle exec jekyll serve --config _config.yml,_config_dev.yml -D -I
+
+bundle exec jekyll build --config _config.yml,_config_dev.yml
+asciidoctor-pdf EvgenyShepelyukCV.adoc -D _site/
+
+start _site/index.html
